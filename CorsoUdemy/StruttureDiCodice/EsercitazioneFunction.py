df109d09367b37de26d8c4caafed8ac7c435571d
beniamino <beniamino.fuoco@gmail.com>
2020-06-15 15:51:07 +0200

    Creazione della funzione sottrazione

---
+++ CorsoUdemy/StruttureDiCodice/EsercitazioneFunction.py
@@ -0,0 +1,19 @@
+# Devo creare una funzione che prende come parametri due liste (l1 e l2)
+# La seconda lista deve essere un parametro opzionale con i seguenti valori di default [1,2,3,4,5]
+# La funzione deve ritornare la differenza tra le due liste, cioè la lista ritornata deve contenere tutti
+# gli elementi di l1 che non sono presenti in l2
+
+def sottrazione(l1, l2=[1, 2, 3, 4, 5]):
+    result = list()
+    flg = False
+
+    for x in l1:
+        flg = False
+        for y in l2:
+            if x == y:
+                flg = True
+                break
+        if not flg:
+            result.append(x)
+
+    return result
